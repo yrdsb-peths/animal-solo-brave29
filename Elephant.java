ce1cc84d2bcd2a3f48fdef7dacf059bc44a5f5fc
brave29 <147207172+brave29@users.noreply.github.com>
2025-04-29 14:21:34 -0400

    get apple to remove after eaten and respawn

--- Elephant.java
+++ Elephant.java
@@ -12,9 +12,34 @@ public class Elephant extends Actor
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act()
     {
         // Add your action code here.
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        // Remove apple if elephant eats it
+        eat();
+    }
+    
+    /**
+     * Eats the apple and spawn the new apple if an apple is eaten.
+     */
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
+        removeTouching(Apple.class);
     }
 }
