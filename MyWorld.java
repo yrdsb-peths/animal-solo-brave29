b9c9a46a37c28076af4e5643259d87afc6a81a0d
brave29 <147207172+brave29@users.noreply.github.com>
2025-04-29 14:27:20 -0400

    add score label

--- MyWorld.java
+++ MyWorld.java
@@ -7,13 +7,31 @@ import greenfoot.*;
  */
 
 public class MyWorld extends World {
+    
+    public int score = 0;
+    Label scoreLabel;
     public MyWorld() {
         super(600, 400, 1);
         
+        //create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
+        
+        //create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
         
         createApple();
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     /**
