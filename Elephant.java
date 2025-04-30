ccc5d04e4fc8170122805281267a02cde2d8a4ae
brave29 <147207172+brave29@users.noreply.github.com>
2025-04-30 13:44:34 -0400

    add sound to elephant

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephant-225994.mp3");
+
     public Elephant()
     {
         setImage("images/elephant.png");
@@ -44,6 +42,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.spawnApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
